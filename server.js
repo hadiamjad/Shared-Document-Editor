2114c63d950b671e4b0457652070a3e67c50e349
Haadi <Hadiyoville25@gmail.com>
2020-03-16 15:31:54 +0500

    initial commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const io = require('socket.io')(3000)
 
 io.on('connection', socket =>{
     socket.on('msg-to-server', textChanges => {
-        console.log(textChanges)
         socket.broadcast.emit('chat-message', textChanges)
     })
 })
