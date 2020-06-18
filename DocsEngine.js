34248f393140a8989537b43ec52d3b42ca56d7d0
Haadi <Hadiyoville25@gmail.com>
2020-06-18 11:46:06 +0500

    initial commit

--- DocsEngine.js
+++ DocsEngine.js
@@ -9,126 +9,126 @@ const dbConfig = {
     password: "123"
 }
 
-class Document{
-    constructor(I,T,D=null,E=null,S,SU=null){
-        this.id= I,
-        this.Title=T;
-        this.Description=D;
-        this.Email=E;
-        this.SocketIO=S;
-        this.SharedUsersList=SU;
+class Document {
+    constructor(I, T, D = null, E = null, S, SU = null) {
+        this.id = I,
+            this.Title = T;
+        this.Description = D;
+        this.Email = E;
+        this.SocketIO = S;
+        this.SharedUsersList = SU;
     }
 
-    generateHtmlforHomepage(){
-        var html="<div class='Box' id='"+this.id+"'>"
-        html+="<div class='Title'>" +this.Title+"</div>";
-        html+="<div class='Icons row ml-auto'><div class='Share col-sm-1' id='" + this.id + "' data-toggle='modal' data-target='#myModel'><i class='fas fa-user-friends'></i></div><div class='Delete col-sm-1'  id='"+this.id+"' ><i class='fas fa-trash-alt'></i></div></div></div>"
-    
+    generateHtmlforHomepage() {
+        var html = "<div class='Box' id='" + this.id + "'>"
+        html += "<div class='Title'>" + this.Title + "</div>";
+        html += "<div class='Icons row ml-auto'><div class='Share col-sm-1' id='" + this.id + "' data-toggle='modal' data-target='#myModel'><i class='fas fa-user-friends'></i></div><div class='Delete col-sm-1'  id='" + this.id + "' ><i class='fas fa-trash-alt'></i></div></div></div>"
+
         return html;
     }
 
-    generateHtmlforHomepage2(){
-        var html="<div class='Box' id='"+this.id+"'>"
-        html+="<div class='Title'>" +this.Title+"</div>";
-        html+="<div class='Icons row ml-auto'></div>"
-    
+    generateHtmlforHomepage2() {
+        var html = "<div class='Box' id='" + this.id + "'>"
+        html += "<div class='Title'>" + this.Title + "</div>";
+        html += "<div class='Icons row ml-auto'></div>"
+
         return html;
     }
 }
 
-class Homepage{
-    constructor(){
-        this.Documents=[];
-        this.status="NotLoaded";
-        
+class Homepage {
+    constructor() {
+        this.Documents = [];
+        this.status = "NotLoaded";
+
     }
-    loadDocuments(str, cb){
-        var loadDocumentsHelper = function(result){
-            this.Documents=[]
-            for(var c=0;c<result.rowsAffected[0];c++){
-                this.Documents[c]=new Document(result.recordset[c].DocID,result.recordset[c].title, result.recordset[c].dataTxt,result.recordset[c].creator,result.recordset[c].socket);
-            }            
+    loadDocuments(str, cb) {
+        var loadDocumentsHelper = function (result) {
+            this.Documents = []
+            for (var c = 0; c < result.rowsAffected[0]; c++) {
+                this.Documents[c] = new Document(result.recordset[c].DocID, result.recordset[c].title, result.recordset[c].dataTxt, result.recordset[c].creator, result.recordset[c].socket);
+            }
             cb(this.Documents);
-           
+
         }
-                  //hadis chunk
-                  loadDocumentsHelper = loadDocumentsHelper.bind(this);
+        //hadis chunk
+        loadDocumentsHelper = loadDocumentsHelper.bind(this);
 
-                  var con1 = new sql.ConnectionPool(dbConfig)
-                  var req1 = new sql.Request(con1)
-      
-                  con1.connect(function(err){
-                      if(err){
-                          console.log(err)
-                          return
-                      }
-                      req1.query("EXEC retDocs1 @e = \""+str+"\"", function(err,res){
-                          if(err){
-                              console.log('Query')
-                              error = 1
-                          }
-                          else{
+        var con1 = new sql.ConnectionPool(dbConfig)
+        var req1 = new sql.Request(con1)
 
-                               loadDocumentsHelper(res)     
-                          }
-                      con1.close()
-                      })
-                  })
-      
-                  
-                  this.status="Loaded";
-                  eventEmitter.emit("Loaded");
-                   
+        con1.connect(function (err) {
+            if (err) {
+                console.log(err)
+                return
+            }
+            req1.query("EXEC retDocs1 @e = \"" + str + "\"", function (err, res) {
+                if (err) {
+                    console.log('Query')
+                    error = 1
+                }
+                else {
+
+                    loadDocumentsHelper(res)
+                }
+                con1.close()
+            })
+        })
+
+
+        this.status = "Loaded";
+        eventEmitter.emit("Loaded");
+
     }
 
-    loadcollabDocuments(str, cb){
-        var loadDocumentsHelper2 = function(result){
-            this.Documents=[]
-            for(var c=0;c<result.rowsAffected[0];c++){
-                this.Documents[c]=new Document(result.recordset[c].DocID,result.recordset[c].title, result.recordset[c].dataTxt,result.recordset[c].creator,result.recordset[c].socket);
-            }            
+    loadcollabDocuments(str, cb) {
+        var loadDocumentsHelper2 = function (result) {
+            this.Documents = []
+            for (var c = 0; c < result.rowsAffected[0]; c++) {
+                this.Documents[c] = new Document(result.recordset[c].DocID, result.recordset[c].title, result.recordset[c].dataTxt, result.recordset[c].creator, result.recordset[c].socket);
+            }
             cb(this.Documents);
-           
+
         }
-                  //hadis chunk
-                  loadDocumentsHelper2 = loadDocumentsHelper2.bind(this);
+        //hadis chunk
+        loadDocumentsHelper2 = loadDocumentsHelper2.bind(this);
 
-                  var con1 = new sql.ConnectionPool(dbConfig)
-                  var req1 = new sql.Request(con1)
-      
-                  con1.connect(function(err){
-                      if(err){
-                          console.log(err)
-                          return
-                      }
-                      req1.query("EXEC retDocs @e = \""+str+"\"", function(err,res){
-                          if(err){
-                              console.log('Query inside collab')
-                              error = 1
-                          }
-                          else{
+        var con1 = new sql.ConnectionPool(dbConfig)
+        var req1 = new sql.Request(con1)
 
-                               loadDocumentsHelper2(res)     
-                          }
-                      con1.close()
-                      })
-                  })
-      
-                  
-                  this.status="Loaded";
-                  eventEmitter.emit("Loaded");
-                   
+        con1.connect(function (err) {
+            if (err) {
+                console.log(err)
+                return
+            }
+            req1.query("EXEC retDocs @e = \"" + str + "\"", function (err, res) {
+                if (err) {
+                    console.log('Query inside collab')
+                    error = 1
+                }
+                else {
+
+                    loadDocumentsHelper2(res)
+                }
+                con1.close()
+            })
+        })
+
+
+        this.status = "Loaded";
+        eventEmitter.emit("Loaded");
+
     }
 
-    load(){
+    load() {
         this.loadDocuments();
     }
-    on(e,cb){
-        eventEmitter.on(e,cb);
+    on(e, cb) {
+        eventEmitter.on(e, cb);
     }
 }
 
-module.exports={
-    Document:Document,
-    Homepage:Homepage
+module.exports = {
+    Document: Document,
+    Homepage: Homepage
 };
--- expS.js
+++ expS.js
@@ -2,9 +2,8 @@ const express = require('express')
 const fs = require('fs');
 const bodyParser = require('body-parser')
 const path = require('path')
-var getDocs = require('./DocsEngine.js');
-
-
+var getDocs = require('C:/Users/Haadi/Desktop/latest project/Shared-Document-Editor/model/DocsEngine.js');
+const nodemailer = require('nodemailer');
 
 var homepage = new getDocs.Homepage();
 
@@ -22,128 +21,134 @@ var app = express()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var io = new Array(300)
-for(i=0;i<300;i++){
-    io[i] = require('socket.io')(5000+i);
-    io[i].on('connection', socket =>{
+for (i = 0; i < 300; i++) {
+    io[i] = require('socket.io')(5000 + i);
+    io[i].on('connection', socket => {
         socket.on('msg-to-server', data => {
-            setDbms(data.textChanges,data.docID);
+            setDbms(data.textChanges, data.docID);
             socket.broadcast.emit('chat-message', data.textChanges)
+        })
+        socket.on('mouse-activity', data=>{
+            //console.log(data)
+            socket.broadcast.emit('cursor', data)
         })
     })
 }
+////////////////////////////////////////////////////////////////
 
-function setDbms(textChanges,docid){
+//////////////////////////////////////////////////////////////////
+function setDbms(textChanges, docid) {
     var con = new sql.ConnectionPool(dbConfig)
     var req1 = new sql.Request(con)
-    con.connect(function(err){
-        if(err){
+    con.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
-        req1.query("UPDATE Document SET dataTxt = '"+textChanges+"' where DocID="+docid+" ", function(err,res){
-            if(err){
+        req1.query("UPDATE Document SET dataTxt = '" + textChanges + "' where DocID=" + docid + " ", function (err, res) {
+            if (err) {
                 console.log('Query')
                 error = 1
             }
-            else{
+            else {
                 //console.log("successful doc textbox")
             }
-            
-        con.close()
+
+            con.close()
         })
     })
 }
 
 // Login landing page is loded
-app.get(["/","/Login"],function (request,response) {	  
-	filePath = "Login.html"
+app.get(["/", "/Login"], function (request, response) {
+    filePath = "view/Login.html"
 
-	fs.readFile(filePath,function(err,contents){		
-		response.send(contents.toString())
-	})
+    fs.readFile(filePath, function (err, contents) {
+        response.send(contents.toString())
+    })
 })
 
 // Register landing page is loded
-app.get(["/Register"],function (request,response) {	  
-	filePath = "Register.html"
+app.get(["/Register"], function (request, response) {
+    filePath = "view/Register.html"
 
-	fs.readFile(filePath,function(err,contents){		
-		response.send(contents.toString())
-	})
+    fs.readFile(filePath, function (err, contents) {
+        response.send(contents.toString())
+    })
 })
 
 // editor landing page is loded
-app.get(["/editor"],function (request,response) {	  
+app.get(["/editor"], function (request, response) {
     var con1 = new sql.ConnectionPool(dbConfig)
     var req1 = new sql.Request(con1)
     var dbout;
-    con1.connect(function(err){
-        if(err){
+    con1.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
-        req1.query("EXEC getDocs @ID = "+ClickedDoc+" ", function(err,res){
-            if(err){
+        req1.query("EXEC getDocs @ID = " + ClickedDoc + " ", function (err, res) {
+            if (err) {
                 console.log('Query')
                 error = 1
             }
-            else{
+            else {
 
-                value = "var user = \""+CurrentUser +"\"";
-                docid = "var docid = "+ClickedDoc +"";
-                socket = "const socket = io('http://localhost:"+res.recordset[0].socket+"')";
-                scripttag = "<script defer src = \"http://localhost:"+res.recordset[0].socket+"/socket.io/socket.io.js\"></script>";
-                data = 'var data = \''+String(res.recordset[0].dataTxt) +'\'';
-                doc_name = "<input id=\"Name_document\"  value=\""+res.recordset[0].title+"\" >"
+                value = "var user = \"" + CurrentUser + "\"";
+                docid = "var docid = " + ClickedDoc + "";
+                socket = "const socket = io('http://localhost:" + res.recordset[0].socket + "')";
+                scripttag = "<script defer src = \"http://localhost:" + res.recordset[0].socket + "/socket.io/socket.io.js\"></script>";
+                data = 'var data = \'' + String(res.recordset[0].dataTxt) + '\'';
+                doc_name = "<input id=\"Name_document\"  value=\"" + res.recordset[0].title + "\" >"
                 CurrentUser = null;
                 ClickedDoc = null;
-                
-                filePath = "index.html"
-                
-            
-                fs.readFile(filePath,function(err,contents){		
-                    contents= contents.toString().replace("/*username*/",value)	
-                    contents= contents.toString().replace("/*docid*/",docid)	
-                    contents= contents.toString().replace("/*socket*/",socket)
-                    contents= contents.toString().replace("/*data*/",data)
-                    contents= contents.toString().replace("<!-- Name_document -->",doc_name)
-                    contents= contents.toString().replace("<!-- script -->",scripttag)
-                    response.send(contents);	
+
+                filePath = "view/index.html"
+
+
+                fs.readFile(filePath, function (err, contents) {
+                    contents = contents.toString().replace("/*username*/", value)
+                    contents = contents.toString().replace("/*docid*/", docid)
+                    contents = contents.toString().replace("/*socket*/", socket)
+                    contents = contents.toString().replace("/*data*/", data)
+                    contents = contents.toString().replace("<!-- Name_document -->", doc_name)
+                    contents = contents.toString().replace("<!-- script -->", scripttag)
+                    response.send(contents);
                 })
             }
-            
-        con1.close()
+
+            con1.close()
         })
     })
 
 })
 
 // Document list landing page is loded
-app.get(["/Docs"],function (request,response) {	
-    var sharedDocs = " "; 
-    homepage.loadcollabDocuments(CurrentUser,function(docs){
-        for(i=0; i< docs.length;i++){
-            sharedDocs+=docs[i].generateHtmlforHomepage2();
+app.get(["/Docs"], function (request, response) {
+    var sharedDocs = " ";
+    homepage.loadcollabDocuments(CurrentUser, function (docs) {
+        for (i = 0; i < docs.length; i++) {
+            sharedDocs += docs[i].generateHtmlforHomepage2();
         }
     })
-    homepage.loadDocuments(CurrentUser,function(docs){
+    homepage.loadDocuments(CurrentUser, function (docs) {
         var html = ""
 
-        for(i=0; i< docs.length;i++){
-            html+=docs[i].generateHtmlforHomepage();
+        for (i = 0; i < docs.length; i++) {
+            html += docs[i].generateHtmlforHomepage();
         }
 
-        filePath = "Docx.html"
-        value = "var user = \""+CurrentUser +"\"";
+        filePath = "view/Docx.html"
+        value = "var user = \"" + CurrentUser + "\"";
         CurrentUser = null;
 
-       fs.readFile(filePath,function(err,contents){	
-            contents = contents.toString().replace("<!-- content -->",html);
-            contents = contents.toString().replace("<!-- sharedDocs -->",sharedDocs);
-            contents= contents.toString().replace("/*username*/",value)	
+        fs.readFile(filePath, function (err, contents) {
+            contents = contents.toString().replace("<!-- content -->", html);
+            contents = contents.toString().replace("<!-- sharedDocs -->", sharedDocs);
+            contents = contents.toString().replace("/*username*/", value)
             response.send(contents);
         });
- 
+
     });
 })
 
@@ -158,17 +163,17 @@ app.post('/deleteDocs', urlencodedParser, function (req, response) {
     var con1 = new sql.ConnectionPool(dbConfig)
     var req1 = new sql.Request(con1)
 
-    con1.connect(function(err){
-        if(err){
+    con1.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
-        req1.query("EXEC delDocs @ID = "+req.body.id+" ", function(err,res){
-            if(err){
+        req1.query("EXEC delDocs @ID = " + req.body.id + " ", function (err, res) {
+            if (err) {
                 console.log('Query')
                 error = 1
             }
-        con1.close()
+            con1.close()
         })
     })
     response.send("Succesful /deleteDocs post request ")
@@ -179,17 +184,41 @@ app.post('/collab', urlencodedParser, function (req, response) {
     var con1 = new sql.ConnectionPool(dbConfig)
     var req1 = new sql.Request(con1)
 
-    con1.connect(function(err){
-        if(err){
+    con1.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
-        req1.query("EXEC istCollab @e = \""+req.body.email+"\", @ID = "+req.body.docID+" ", function(err,res){
-            if(err){
+        req1.query("EXEC istCollab @e = \"" + req.body.email + "\", @ID = " + req.body.docID + " ", function (err, res) {
+            if (err) {
                 console.log('Query error in colloaborator')
                 error = 1
             }
-        con1.close()
+            else {
+                //let transport = nodemailer.createTransport(options[defaults])
+                var transport = nodemailer.createTransport({
+                    host: "smtp.mailtrap.io",
+                    port: 2525,
+                    auth: {
+                        user: "f3abc83182bf3f",
+                        pass: "6a26fe81fc9164"
+                    }
+                });
+                const message = {
+                    from: 'notepad@no-reply.com', // Sender address
+                    to: req.body.email,         // List of recipients
+                    subject: 'NotePad Collaborator', // Subject line
+                    text: '' + req.body.creator + ' have shared his docx for collaboration. '// Plain text body
+                };
+                transport.sendMail(message, function (err, info) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log(info);
+                    }
+                });
+            }
+            con1.close()
         })
     })
     response.send("Succesful /collab post request ")
@@ -200,21 +229,21 @@ app.post('/collabList', urlencodedParser, function (req, response) {
     var con1 = new sql.ConnectionPool(dbConfig)
     var req1 = new sql.Request(con1)
 
-    con1.connect(function(err){
-        if(err){
+    con1.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
-        req1.query("Select * from DocumentEditors where DocsID = "+req.body.docID+" ", function(err,res){
-            if(err){
+        req1.query("Select * from DocumentEditors where DocsID = " + req.body.docID + " ", function (err, res) {
+            if (err) {
                 console.log('Query error in colloaboratorList')
                 error = 1
             }
             response.send(res)
-        con1.close()
+            con1.close()
         })
     })
-    
+
 })
 
 // post request handler for newDoc
@@ -222,17 +251,17 @@ app.post('/newDocs', urlencodedParser, function (req, response) {
     var con1 = new sql.ConnectionPool(dbConfig)
     var req1 = new sql.Request(con1)
 
-    con1.connect(function(err){
-        if(err){
+    con1.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
-        req1.query("EXEC istDocs @e =\""+req.body.email+"\"", function(err,res){
-            if(err){
+        req1.query("EXEC istDocs @e =\"" + req.body.email + "\"", function (err, res) {
+            if (err) {
                 console.log('Query in')
                 error = 1
             }
-        con1.close()
+            con1.close()
         })
     })
     response.send("Succesful /newDoc post request ")
@@ -244,17 +273,17 @@ app.post('/nameChange', urlencodedParser, function (req, response) {
     var con1 = new sql.ConnectionPool(dbConfig)
     var req1 = new sql.Request(con1)
 
-    con1.connect(function(err){
-        if(err){
+    con1.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
-        req1.query("UPDATE Document SET title = '"+req.body.value+"' where DocID="+req.body.docID+" ", function(err,res){
-            if(err){
+        req1.query("UPDATE Document SET title = '" + req.body.value + "' where DocID=" + req.body.docID + " ", function (err, res) {
+            if (err) {
                 console.log('Query in')
                 error = 1
             }
-        con1.close()
+            con1.close()
         })
     })
     response.send("Succesful /deleteDocs post request ")
@@ -265,17 +294,17 @@ app.post('/remCollab', urlencodedParser, function (req, response) {
     var con1 = new sql.ConnectionPool(dbConfig)
     var req1 = new sql.Request(con1)
 
-    con1.connect(function(err){
-        if(err){
+    con1.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
-        req1.query("Delete from DocumentEditors where email = '"+req.body.email+"' and DocsID="+req.body.docID+" ", function(err,res){
-            if(err){
+        req1.query("Delete from DocumentEditors where email = '" + req.body.email + "' and DocsID=" + req.body.docID + " ", function (err, res) {
+            if (err) {
                 console.log('Query in')
                 error = 1
             }
-        con1.close()
+            con1.close()
         })
     })
     response.send("Succesful /deleteDocs post request ")
@@ -295,8 +324,8 @@ app.post('/login', urlencodedParser, function (req, resp) {
 
     var flag = 0; // 1 for error 0 for !error
 
-    con1.connect(function(err){
-        if(err){
+    con1.connect(function (err) {
+        if (err) {
             console.log(err)
             return
         }
@@ -326,7 +355,7 @@ app.post('/login', urlencodedParser, function (req, resp) {
                     //con1.close()
                 }
                 else {
-                    req1.query("EXEC LoginReq @pp = \""+req.body.Password+"\", @e = \""+req.body.Email+"\"", function (err, res) {
+                    req1.query("EXEC LoginReq @pp = \"" + req.body.Password + "\", @e = \"" + req.body.Email + "\"", function (err, res) {
                         if (err) {
                             console.log('Login check SQL Error1')
                             error = 1
@@ -356,7 +385,7 @@ app.post('/register', urlencodedParser, function (req, resp) {
     console.log(req.body)
     var flag = 0; // 1 for error 0 for !error
 
-    con1.connect(function(err){
+    con1.connect(function (err) {
         /*if(err){
             console.log(err)
             return
@@ -395,7 +424,7 @@ app.post('/register', urlencodedParser, function (req, resp) {
 //server start notification
 var server = app.listen(3000, () => {
     console.log("Server is listnening on the port 3000");
-   });
+});
 
 // static files loaded
 app.use("/static", express.static("static"));
